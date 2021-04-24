c43f56ed3c44c80edf33bac1a45b1412414bd980
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2021-04-24 16:57:09 +0200

    updated controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
     def new
         @user = User.new
+    end
+
+    def show
+        @user = User.find(params[:id])
     end
 
     def create
@@ -8,11 +15,28 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-            redirect_to new_user_path
+            redirect_to user_path(@user)
         else
             render :new
          end
     end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update
+            flash.notice("#{@user.username} has been edited!")
+            redirect_to user_path(@user)
+        else
+            render :edit
+        end
+
+    end
+
+    private
 
     def user_params
         params.permit(:username, :email, :password)
